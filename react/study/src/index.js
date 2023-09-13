e8be2b5e659f5eacb15709fba6f7bff2eab4035b
Cat0the0meow <144585558+Cat0the0meow@users.noreply.github.com>
2023-09-13 05:16:36 +0000

    react

--- react/study/src/index.js
+++ react/study/src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-// import App from './input_state_1/App';
-// import App from './components_2/App';
-// import App from './array_data_3/App';
-// import App from './shared_state_4/App';
-// import App from './styled_component_5/App';
-// import App from './context_7/App';
-// import App from './counter_8/App';
-// import App from './routerV6_9/App';
-//import App from './routerEx/App';
-//import App from './reducer_10/App';
-import App from './customHook_11/App';
+// import App from './01input_state/App';
+// import App from './02components/App';
+// import App from './03array_data/App';
+// import App from './04shared_state/App';
+// import App from './05styled_component/App';
+// import App from './06context/App';
+// import App from './07counter/App';
+// import App from './08routerV6/App';
+//import App from './09routerEx/App';
+//import App from './10reducer/App';
+import App from './11customHook/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
